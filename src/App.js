7fa788b970a4da956f96c9c6f419633ac2a39d29
TataBakhturidze <bakhturidzetata@gmail.com>
2021-04-26 21:14:56 +0400

    mutacia dalagda proeqti

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useEffect} from 'react'
 import { Switch, Route, useHistory } from 'react-router'
 import {firebase, auth} from "./firebase/Configuration"
@@ -6,15 +5,10 @@ import Registration from "./pages/Registration"
 import LogIn from "./pages/LogIn"
 import Dashboard from "./pages/Dashboard"
 import ForgotPassword from './pages/ForgotPassword'
-
-=======
 import './App.css';
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
-import Logo from './photos/logo7.png'
-import Colors from './photos/colors.jpg'
 import Main from './components/main/main';
->>>>>>> 0521a8d8315745668a1954719a5bea843d23a4e3
 
 function App() {
 
@@ -32,22 +26,18 @@ function App() {
 }, [])
 
   return (
-<<<<<<< HEAD
-    <Switch>
-      <Route path="/" exact component={Registration}></Route>
-      <Route path="/sign-in" component={LogIn} exact></Route>
-      <Route path="/dashboard" component={Dashboard} exact></Route>
-      <Route path="/forgot-password" component={ForgotPassword} exact></Route>
-    </Switch>
-  )
-=======
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+        <Header/>
+      <Switch>
+        <Route path="/" exact component={Registration}></Route>
+        <Route path="/sign-in" component={LogIn} exact></Route>
+        <Route path="/dashboard" component={Dashboard} exact></Route>
+        <Route path="/forgot-password" component={ForgotPassword} exact></Route>
+      </Switch>
+        <Footer/>
     </div>
-  );
->>>>>>> 0521a8d8315745668a1954719a5bea843d23a4e3
+  )
+  
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
-import {BrowserRouter as Router} from "react-router-dom"
 import store from "./redux/store"
 import { Provider } from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
     <Provider store={store}>
       <App />
     </Provider>
-=======
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom'
-
-ReactDOM.render(
-  <Router>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> 0521a8d8315745668a1954719a5bea843d23a4e3
   </Router>,
   document.getElementById('root')
 );
 
-<<<<<<< HEAD
-=======
-reportWebVitals();
->>>>>>> 0521a8d8315745668a1954719a5bea843d23a4e3
+
