66c255c2fc840448342d40125d99bc04a975c148
TataBakhturidze <bakhturidzetata@gmail.com>
2021-05-15 14:03:01 +0400

    language

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -28,7 +28,7 @@ function Profile() {
     const {t} = useTranslation()
 
     const bold = {
-        color: "#001e3f",
+      color: "#034488",
         fontWeight: "bold"
     }
 
@@ -39,10 +39,10 @@ function Profile() {
     const {isLoggedIn} = useSelector(state => state)
 
     const route = () => {
-        if (pathName == "/profile/settings" || pathName == "/profile") return "Edit Profile"
-        if (pathName == "/profile/orders") return "My Orders"
-        if (pathName == "/profile/payments") return "Payment Methods"
-        if (pathName == "/profile/addresses") return "Delivery Adresses"
+        if (pathName == "/profile/settings" || pathName == "/profile") return `${t('Edit profile')}`
+        if (pathName == "/profile/orders") return `${t('My orders')}`
+        if (pathName == "/profile/payments") return `${t('Payment methods')}`
+        if (pathName == "/profile/addresses") return `${t('Delivery adresses')}`
     }
     useEffect(() => {
       if(isLoggedIn === true && pathName == path || pathName == `${path}/`) history.push("/profile/settings")
@@ -52,30 +52,30 @@ function Profile() {
         <div className="profile">
         <p className="urlShowPProfile">Profile > <span>{route()}</span></p>
         <div className="profileFormUrls">
-        <div className="profileUrls">
-        <ul >
-        <li>
-        <Link style={pathName == "/profile/settings" ? bold : {}}  to={`${url}/settings`}>
-          <EditOutlinedIcon/><p>{t('Edit Profile')}</p>
-        </Link>
-        </li>
-        <li>
-        <Link style={pathName === "/profile/orders" ? bold : {}}  to={`${url}/orders`}>
-          <DescriptionOutlinedIcon/><p>{t('My Orders')}</p>
-        </Link>
-        </li>
-        <li>
-        <Link style={pathName === "/profile/payments" ? bold : {}}  to={`${url}/payments`}>
-          <PaymentOutlinedIcon/><p>{t('Payment Methods')}</p>
-        </Link>
-        </li>
-        <li>
-        <Link style={pathName === "/profile/addresses" ? bold: {}}  to={`${url}/addresses`}>
-          <HomeOutlinedIcon/><p>{t('Delivery Adresses')}</p>
-        </Link>
-        </li>
-      </ul>
-      </div>
+          <div className="profileUrls">
+            <ul >
+              <li>
+                <Link style={pathName == "/profile/settings" ? bold : {}}  to={`${url}/settings`}>
+                  <EditOutlinedIcon/><p>{t('Edit Profile')}</p>
+                </Link>
+              </li>
+              <li>
+                <Link style={pathName === "/profile/orders" ? bold : {}}  to={`${url}/orders`}>
+                  <DescriptionOutlinedIcon/><p>{t('My Orders')}</p>
+                </Link>
+              </li>
+              <li>
+                <Link style={pathName === "/profile/payments" ? bold : {}}  to={`${url}/payments`}>
+                  <PaymentOutlinedIcon/><p>{t('Payment Methods')}</p>
+                </Link>
+              </li>
+              <li>
+                <Link style={pathName === "/profile/addresses" ? bold: {}}  to={`${url}/addresses`}>
+                  <HomeOutlinedIcon/><p>{t('Delivery Adresses')}</p>
+                </Link>
+              </li>
+          </ul>
+        </div>
 
       <Switch>
         <ProtectedRoute path={`${path}`} component={Settings} exact></ProtectedRoute>
