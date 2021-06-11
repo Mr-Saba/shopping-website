a2f759227bd2b5f7553ec679e0dc926e87511249
TataBakhturidze <bakhturidzetata@gmail.com>
2021-06-11 21:45:51 +0400

    main page done about us done terms done

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,10 +1,60 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import Death from '../../photos/1.jpg'
+import Death2 from '../../photos/yinyang1.jpg'
+import Death3 from '../../photos/heartwitheye1.jpg'
+import Death4 from '../../photos/4.jpg'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image, ImageWithZoom } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import './main.css'
+import Products from '../../pages/production/Products'
+
 function Main() {
+
     return (
         <div className="main">
-            {/* <img src={Colors} alt=""/> */}nothing
+            <div className="sliderAndContent">
+                <Link to="/production">
+                <CarouselProvider
+                    naturalSlideWidth={100}
+                    naturalSlideHeight={100}
+                    totalSlides={4}
+                    playDirection='forward'
+                    isPlaying={true}
+                    interval={5000}
+                    visibleSlides={2}
+                >
+                    <div className="container">
+                    <Slider>
+                        <Slide index={0}><Image src={Death} /></Slide>
+                        <Slide index={1}><Image src={Death2} /></Slide>
+                        <Slide index={2}><Image src={Death3} /></Slide>
+                        <Slide index={3}><Image src={Death4} /></Slide>
+                    </Slider>
+                    </div>
+                </CarouselProvider>
+                </Link>
+                <div className="contentMain">
+                        <p>Be original,
+                            Be YOURSELF
+                        </p>
+                        <Link to="/production">
+                            <span>Shop now <ArrowForwardIcon/></span>
+                        </Link>
+                </div>
+            </div>
+                <div className="mainProductsLine">
+                    <div className="mainPageProductsTitle">
+                        <p className="title">Products</p>
+                        <Link to="/production">
+                            <span>See all <ArrowForwardIcon/></span>
+                        </Link>
+                    </div>
+                    <div className="productionGridMain">
+                        <Products/>
+                    </div>
+                </div>
         </div>
     )
 }
--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import './aboutUs.css'
 function AboutUs() {
 
     return (
-        <div>about</div>
+        <div className="aboutUs">
+            <p>About us</p>
+            <span>
+            Most of people don't realize and think the meaning of existence because it is pretty hard to find out what they are. But I want to seek what I am and figure out the meaning of living with reasons at this time.
+             The first reason I live is because God created me and my parents gave me a birth. There is an important truth that I am born in this world, which is the continuance of life. God sent me to my parents to let human beings keep going on, which means I have a purpose to connect the past and the present. This is the main reason that I can be existing since I was born.
+             The second reason I live is because I should make my own history until I die. As people draw their own history, either negatively or affirmatively, on the large paper called 'life', I should do the same thing too. It looks like a simple matter, but it is the most complicated mission that God has given to all mankind.
+             The third reason I live is because I want to contribute my life to volunteering for the world, which is the process of making my own history affirmatively. Many people get lost finding the way to achieve the goal, falling into the negative world. Unlike them, to achieve the goal in the positive way is so valuable that I can be proud of myself.
+             It is certain that nobody can answer the reason why they live easily, but I tried to answer it with an enthusiastic attitude. I think it's meaningful to live a life with reasons because it offers strength to overcome the hardships coming in my whole life and an identity that is distinguished from others. I found out that there is none like me from realizing my own reasons to live. Knowing that, I'm about to start a new life.
+            </span>
+        </div>
     )
 }
 
